4aa93dcad037d3b65df8235b8840fc268e38f337
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-01-03 13:38:49 +0530

    Added function to close server

--- SimpleStaticFilesServer/index.js
+++ SimpleStaticFilesServer/index.js
@@ -4,6 +4,13 @@ var port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
+var server = app.listen(port, function () {
     console.log(`App listening at ${port}`);
+});
+
+process.on('SIGINT', function () {
+    console.log('Closing app');
+    server.close(function () {
+        console.log('App closed');
+    });
 });
