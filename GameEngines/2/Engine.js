9e2232d82605a3878d696601b80b93196445440b
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-01-05 14:19:32 +0530

    Added GameEngine

---
+++ GameEngines/2/Engine.js
@@ -0,0 +1,51 @@
+var Engine = {};
+
+(function () {
+
+    Engine.create = function () {
+
+        var engine = {
+            frameRequestId: undefined,
+            time: undefined,
+            ms_per_update: 1000 / 60
+        };
+
+        return engine;
+    };
+
+    Engine.start = function (engine) {
+        if (engine.time === undefined) { engine.time = Date.now(); }
+
+        (function loop() {
+            var time = Date.now();
+            var isUpdateRequired = (time > engine.time);
+
+            if (isUpdateRequired) {
+                Events.trigger(engine, 'beforeTick', engine);
+            }
+
+            while (time > engine.time) {
+                Events.trigger(engine, 'beforeUpdate', engine);
+                Events.trigger(engine, 'update', engine);
+                Events.trigger(engine, 'afterUpdate', engine);
+                engine.time += engine.ms_per_update;
+            }
+
+            if (isUpdateRequired) {
+                Events.trigger(engine, 'beforeRender', engine);
+                Events.trigger(engine, 'render', engine);
+                Events.trigger(engine, 'afterRender', engine);
+                Events.trigger(engine, 'afterTick', engine);
+            }
+
+            engine.frameRequestId = window.requestAnimationFrame(function () { loop(); });
+        })();
+    };
+
+    Engine.stop = function (engine) {
+        window.cancelAnimationFrame(engine.frameRequestId);
+        engine.frameRequestId = undefined;
+        engine.time = undefined;
+    };
+
+})();
